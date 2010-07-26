ae2e45dc8c4e61bcfde67c16d1e9a5148e6d68a0
bigsean <sclifford@gmail.com>
2010-07-26 17:36:04 -0500

    Set target to OS 4.0.x and removed icons not needed at this point.

---
+++ Classes/LauncherViewController.m
@@ -0,0 +1,14 @@
+//
+//  LauncherViewController.m
+//  landscapes
+//
+//  Created by Sean Clifford on 7/26/10.
+//  Copyright 2010 National Park Service/NCPTT. All rights reserved.
+//
+
+#import "LauncherViewController.h"
+
+
+@implementation LauncherViewController
+
+@end
