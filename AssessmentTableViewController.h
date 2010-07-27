816f424c1dcd312d8e97b0c3b0861aad08cffdde
bigsean <sclifford@gmail.com>
2010-07-27 16:40:38 -0500

    Changed copyright information in a few files to National Park Service/NCPTT.

--- AssessmentTableViewController.h
+++ AssessmentTableViewController.h
@@ -3,7 +3,7 @@
 //  landscapes
 //
 //  Created by Evan Cordell on 7/26/10.
-//  Copyright 2010 NCPTT. All rights reserved.
+//  Copyright National Park Service/NCPTT 2010. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
--- Classes/Assessment.m
+++ Classes/Assessment.m
@@ -3,7 +3,7 @@
 //  landscapes
 //
 //  Created by Evan Cordell on 7/27/10.
-//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//  Copyright National Park Service/NCPTT 2010. All rights reserved.
 //
 
 #import "Assessment.h"
