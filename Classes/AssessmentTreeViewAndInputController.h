83e271cae052e10fd6dbf9bf1e0592df995c6950
ecordell <cordell.evan@gmail.com>
2010-08-02 15:33:29 -0500

    Commit to save work before reverting to get AssessmentTree to load

--- Classes/AssessmentTreeViewAndInputController.h
+++ Classes/AssessmentTreeViewAndInputController.h
@@ -8,12 +8,48 @@
 
 #import <UIKit/UIKit.h>
 #import "Assessment.h"
+#import "AssessmentTree.h"
+#import "AppDelegate.h"
 
-@interface AssessmentTreeViewAndInputController : UIViewController {
+@interface AssessmentTreeViewAndInputController : UIViewController <NSFetchedResultsControllerDelegate> {
     Assessment *assessment;
+    AssessmentTree *assessmentTree;
     IBOutlet UILabel *assessor;
+    IBOutlet UILabel *date;
+    IBOutlet UILabel *formCText;
+    IBOutlet UILabel *crownCText;
+    IBOutlet UILabel *trunkCText;
+    IBOutlet UILabel *rootFlareCText;
+    IBOutlet UILabel *rootsCText;
+    IBOutlet UILabel *overallCText;
+    IBOutlet UILabel *formRText;
+    IBOutlet UILabel *crownRText;
+    IBOutlet UILabel *trunkRText;
+    IBOutlet UILabel *rootFlareRText;
+    IBOutlet UILabel *rootsRText;
+    IBOutlet UILabel *overallRText;
+ @private
+    NSFetchedResultsController *fetchedResultsController;
+    NSManagedObjectContext *managedObjectContext;
 }
 @property (nonatomic, retain) Assessment *assessment;
+@property (nonatomic, retain) AssessmentTree *assessmentTree;
 @property (nonatomic, retain) UILabel *assessor;
+@property (nonatomic, retain) UILabel *date;
+@property (nonatomic, retain) UILabel *formCText;
+@property (nonatomic, retain) UILabel *crownCText;
+@property (nonatomic, retain) UILabel *trunkCText;
+@property (nonatomic, retain) UILabel *rootFlareCText;
+@property (nonatomic, retain) UILabel *rootsCText;
+@property (nonatomic, retain) UILabel *overallCText;
+@property (nonatomic, retain) UILabel *formRText;
+@property (nonatomic, retain) UILabel *crownRText;
+@property (nonatomic, retain) UILabel *trunkRText;
+@property (nonatomic, retain) UILabel *rootFlareRText;
+@property (nonatomic, retain) UILabel *rootsRText;
+@property (nonatomic, retain) UILabel *overallRText;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
 -(id)initWithNavigatorURL:(NSURL*)URL query:(NSDictionary*)query;
 @end
--- Classes/AssessmentTreeViewAndInputController.m
+++ Classes/AssessmentTreeViewAndInputController.m
@@ -10,13 +10,28 @@
 
 
 @implementation AssessmentTreeViewAndInputController
-@synthesize assessment, assessor;
+@synthesize assessment, assessmentTree, assessor, date, fetchedResultsController, managedObjectContext;
+@synthesize formCText, crownCText, trunkCText, rootFlareCText, rootsCText, overallCText;
+@synthesize formRText, crownRText, trunkRText, rootFlareRText, rootsRText, overallRText;
 
 -(id)initWithNavigatorURL:(NSURL*)URL query:(NSDictionary*)query { 
     if (self = [super initWithNibName:@"AssessmentTreeViewAndInput" bundle:[NSBundle mainBundle]]){ 
         if(query && [query objectForKey:@"assessment"]){ 
             self.assessment = (Assessment*) [query objectForKey:@"assessment"]; 
             self.assessor = [[UILabel alloc] init];
+            self.date = [[UILabel alloc] init];
+            self.formCText = [[UILabel alloc] init];
+            self.crownCText = [[UILabel alloc] init];
+            self.trunkCText = [[UILabel alloc] init];
+            self.rootFlareCText = [[UILabel alloc] init];
+            self.rootsCText = [[UILabel alloc] init];
+            self.overallCText = [[UILabel alloc] init];
+            self.formRText = [[UILabel alloc] init];
+            self.crownRText = [[UILabel alloc] init];
+            self.trunkRText = [[UILabel alloc] init];
+            self.rootFlareRText = [[UILabel alloc] init];
+            self.rootsRText = [[UILabel alloc] init];
+            self.overallRText = [[UILabel alloc] init];
         } 
     } 
     return self; 
@@ -35,7 +50,33 @@
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
+    if(!managedObjectContext){
+        managedObjectContext = [(AppDelegate *)[[UIApplication sharedApplication] delegate] managedObjectContext];
+    }
+   //set up fetchedResultsController
+    [self fetchedResultsController];
+    
+    //Perform fetch and catch any errors
+    NSError *error = nil;
+    [fetchedResultsController performFetch:&error];
+    if (error) {
+        NSLog(@"Error occured fetching from db: %@", error);
+    }
+    
+    self.title = @"Tree";
     self.assessor.text = self.assessment.assessor;
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateStyle:NSDateFormatterLongStyle];
+    NSString *dateStr= [dateFormatter stringFromDate:self.assessment.created_at];
+    [dateFormatter release];
+    NSLog(@"%@", dateStr);
+    self.date.text = dateStr;
+    if (self.assessment) {
+        //self.assessmentTree = (AssessmentTree *)[fetchedResultsController objectAtIndexPath:[NSIndexPath indexPathForRow:0 inSection:0]];
+    }
+    //NSLog(@"%@", [fetchedResultsController count]);
+    id <NSFetchedResultsSectionInfo> sectionInfo = [[fetchedResultsController sections] objectAtIndex:0];
+    NSLog(@"Number of Rows fetched: %d", [sectionInfo numberOfObjects]);
 }
 
 
@@ -60,9 +101,60 @@
     // e.g. self.myOutlet = nil;
 }
 
+#pragma mark -
+#pragma mark Fetched results controller
+
+- (NSFetchedResultsController *)fetchedResultsController {
+    // Set up the fetched results controller if needed.
+    if (fetchedResultsController == nil) {
+        // Create the fetch request for the entity.
+        NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+        // Edit the entity name as appropriate.
+        NSEntityDescription *entity = [NSEntityDescription entityForName:@"AssessmentTree" inManagedObjectContext:managedObjectContext];
+        [fetchRequest setEntity:entity];
+        
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"assessment.created_at == %@", self.assessment.created_at];
+        [fetchRequest setPredicate:predicate];
+        
+        // Edit the sort key as appropriate.
+        NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"assessment" ascending:YES];
+        NSArray *sortDescriptors = [[NSArray alloc] initWithObjects:sortDescriptor, nil];
+        
+        [fetchRequest setSortDescriptors:sortDescriptors];
+        
+        // Edit the section name key path and cache name if appropriate.
+        // nil for section name key path means "no sections".
+        NSFetchedResultsController *aFetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest managedObjectContext:managedObjectContext sectionNameKeyPath:nil cacheName:@"assessmentTree_cache"];
+        aFetchedResultsController.delegate = self;
+        self.fetchedResultsController = aFetchedResultsController;
+        
+        [aFetchedResultsController release];
+        [fetchRequest release];
+        [sortDescriptor release];
+        [sortDescriptors release];
+    }
+    
+    return fetchedResultsController;
+}    
 
 - (void)dealloc {
     [assessor release];
+    [date release];
+    [formCText release];
+    [crownCText release];
+    [trunkCText release];
+    [rootFlareCText release];
+    [rootsCText release];
+    [overallCText release];
+    [formRText release];
+    [crownRText release];
+    [trunkRText release];
+    [rootFlareRText release];
+    [rootsRText release];
+    [overallRText release];
+    [fetchedResultsController release];
+    [managedObjectContext release];
+    
     [super dealloc];
 }
 
