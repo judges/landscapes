d02956ea263b373a0745bcf828ead71fa2131dce
bigsean <sclifford@gmail.com>
2010-08-10 15:04:09 -0500

    Adding Landscapes tableview controller and cell classes.

---
+++ Classes/LandscapeTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  LandscapeTableViewCell.h
+//  landscapes
+//
+//  Created by Sean Clifford on 8/10/10.
+//  Copyright 2010 National Park Service/NCPTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LandscapeTableViewCell : UIViewController {
+
+}
+
+@end
---
+++ Classes/LandscapeTableViewController.h
@@ -0,0 +1,16 @@
+//
+//  LandscapeTableViewController.h
+//  landscapes
+//
+//  Created by Sean Clifford on 8/10/10.
+//  Copyright 2010 National Park Service/NCPTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface LandscapeTableViewController : UIViewController {
+
+}
+
+@end
